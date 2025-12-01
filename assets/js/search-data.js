7a2980f3580e8083047e40ff22a974507b28872d
cliu585 <81890935+cliu585@users.noreply.github.com>
2025-12-01 00:39:28 +0000

    Deploying to gh-pages from @ cliu585/parallel-mcts-othello@e53bc305e5e3727055bf236f3f474ab1c21780d7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-midpoint",
+    title: "midpoint",
     section: "Navigation",
     handler: () => {
       window.location.href = "/parallel-mcts-othello/";
